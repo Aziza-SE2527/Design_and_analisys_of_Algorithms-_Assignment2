26a0ee887133b31ed802604ca2ac456408062dc4
Aluana <Muslimova>
2026-09-20 13:01:33 +0500

    deter part 1

--- assignment1-divide-and-conquer/src/DeterministicSelector.java
+++ assignment1-divide-and-conquer/src/DeterministicSelector.java
@@ -1,3 +1,59 @@
 public class DeterministicSelector
 {
+    private long comparisons;
+    private int maxRecursionDepth;
+
+    public int select(int[] arr, int k)
+    {
+        comparisons = 0;
+        maxRecursionDepth = 0;
+        if(arr==null || k<0 ||k>=arr.length)
+        {
+            throw new IllegalArgumentException("Invalid input or k");
+        }
+        int[] copy=arr.clone();
+        return select(copy,0,copy.length-1,k,1);
+    }
+    private int select(int[] arr, int left,int right,int k,int depth)
+    {
+        if(depth> maxRecursionDepth)
+        {
+            maxRecursionDepth = depth;
+        }
+        if(left==right)
+        {
+            return arr[left];
+        }
+        int pivotIndex =medianOfMedians(arr,left,right,depth+1);
+        pivotIndex=partition(arr,left,right,pivotIndex);
+
+        if(k==pivotIndex)
+        {
+            return arr[k];
+        }
+        else if(k<pivotIndex)
+        {
+            return select(arr,left,pivotIndex-1,k,depth+1);
+        }
+        else
+        {
+            return select(arr,pivotIndex+1,right,k,depth+1);
+        }
+    }
+
+    private int medianOfMedians(int[] arr,int left,int right,int k,int depth)
+    {
+        int numElements=right-left+1;
+        if(numElements<=5)
+        {
+            insertionSort(arr,left,right);
+            return left+numElements;
+        }
+        int numGroups=(numElemts+4)/5;
+        for(int i=0; i<numGroups; i++)
+        {
+            int subLeft=left+i*5;
+            int subRight = Math.min(subLeft+4,right);
+        }
+    }
 }
