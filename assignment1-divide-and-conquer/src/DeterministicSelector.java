2e20932858487299233361cb2c0dcbfa4fb5e267
Aluana <Muslimova>
2026-09-20 13:51:18 +0500

    deter part 2

--- assignment1-divide-and-conquer/src/DeterministicSelector.java
+++ assignment1-divide-and-conquer/src/DeterministicSelector.java
@@ -49,11 +49,71 @@ public class DeterministicSelector
             insertionSort(arr,left,right);
             return left+numElements;
         }
-        int numGroups=(numElemts+4)/5;
+        int numGroups=(numElements+4)/5;
         for(int i=0; i<numGroups; i++)
         {
             int subLeft=left+i*5;
             int subRight = Math.min(subLeft+4,right);
+            insertionSort(arr,subLeft,subRight);
+            int medianIndex = subLeft +(subRight-subLeft)/2;
+            swap(arr,left+i,medianIndex);
         }
+        return select(arr,left,left+numGroups-1,left+numGroups/2,depth+1);
+    }
+    private void insertionSort(int[] arr,int left,int right)
+    {
+        for(int i=left+1; i<=right;i++)
+        {
+            int key=arr[i];
+            int j=i-1;
+            while (j>=left)
+            {
+                comparisons++;
+                if (arr[j] > key)
+                {
+                    arr[j+1] = arr[j];
+                    j--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            arr[j+1] = key;
+        }
+    }
+    private int partition(int[] arr,int left,int right,int pivotIndex)
+    {
+        int pivotValue=arr[pivotIndex];
+        swap(arr,pivotIndex,right);
+        int storeIndex=left;
+        for(int i=left+1; i<right;i++)
+        {
+            comparisons++;
+            if(arr[i]>pivotValue)
+            {
+                swap(arr,storeIndex,i);
+                storeIndex++;
+            }
+        }
+        swap(arr,right,storeIndex);
+        return storeIndex;
+    }
+    private void swap(int[] arr,int i,int j)
+    {
+        if(i != j)
+        {
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    }
+    public long getComparisons()
+    {
+        return comparisons;
+    }
+    public int getMaxRecursionDepth()
+    {
+        return maxRecursionDepth;
     }
 }
