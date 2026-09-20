6d07589255aa362d1daeee8c3efbdc3942ed5783
Aluana <Muslimova>
2026-09-20 10:08:03 +0500

    not really sure why its gives errors

--- assignment1-divide-and-conquer/src/Point.java
+++ assignment1-divide-and-conquer/src/Point.java
@@ -1,2 +1,52 @@
-public class Point {
+public class Point implements Comparable<Point>
+{
+    private double x;
+    private double y;
+
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX()
+    {
+        return x;
+    }
+    public double getY()
+    {
+        return y;
+    }
+
+    public double getX()
+    {
+        return x;
+    }
+
+    public double getY()
+    {
+        return y;
+    }
+
+    @Override
+    public int compareTo(Point other)
+    {
+        int cmp = Double.compare(this.x, other.x);
+        if (cmp == 0)
+        {
+            cmp = Double.compare(this.y, other.y);
+        }
+    }
+
+    public double distanceTo(Point other)
+    {
+        double dx = x - other.x;
+        double dy = y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+    @Override
+    public String toString()
+    {
+        return "(" + x + "," + y + ")";
+    }
 }
